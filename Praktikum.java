1b7cb8073d062018a6cc794a512d199c4186093c
SchitoVladimir <schitovladimir@yandex.ru>
2022-09-05 10:16:42 +0300

    Високосный год - v1

--- Praktikum.java
+++ Praktikum.java
@@ -1,7 +1,34 @@
+package first_project;
+import java.util.Scanner;
+
 public class Praktikum {
 
-	public static void main (String[] args) {
-		
-	}
+   public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Введите год:");
+			int year = scanner.nextInt();
 
+			if (isLeapYear(year)) {
+			    System.out.println("Leap year");
+			}
+			else {
+				System.out.println("Not leap year");
+			}
+		}
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(year % 400 == 0) {
+        	return true;
+        }
+        else if(year % 100 == 0) {
+        	return false;
+        }
+        else if(year % 4 == 0) {
+        	return true;
+        }
+        else {
+        	return false;
+        }
+    }
 }
